cfd20e71d46f690fe2789a27ff1a3af6f7fa58e8
Aleksandr Dergunov <dergunovs@mail.ru>
2024-12-16 20:00:25 +0300

    fix: rename base params schema

--- apps/backend/src/modules/common/schema.ts
+++ apps/backend/src/modules/common/schema.ts
@@ -2,22 +2,11 @@ import type { JSONSchemaType } from 'ajv';
 import type { IPaginatedQuery, IBaseParams, IBaseReply } from 'fitness-tracker-contracts';
 
 export const baseParams: JSONSchemaType<IBaseParams> = {
-  $id: 'PaginatedParams',
+  $id: 'BaseParams',
   type: 'object',
   required: ['id'],
   properties: {
     id: { type: 'string' },
-  },
-  $schema: 'http://json-schema.org/draft-07/schema#',
-  additionalProperties: false,
-};
-
-export const paginatedQuery: JSONSchemaType<IPaginatedQuery> = {
-  $id: 'PaginatedQuery',
-  type: 'object',
-  required: ['page'],
-  properties: {
-    page: { type: 'number' },
   },
   $schema: 'http://json-schema.org/draft-07/schema#',
   additionalProperties: false,
@@ -29,6 +18,17 @@ export const baseReply: JSONSchemaType<IBaseReply> = {
   required: ['message'],
   properties: {
     message: { type: 'string' },
+  },
+  $schema: 'http://json-schema.org/draft-07/schema#',
+  additionalProperties: false,
+};
+
+export const paginatedQuery: JSONSchemaType<IPaginatedQuery> = {
+  $id: 'PaginatedQuery',
+  type: 'object',
+  required: ['page'],
+  properties: {
+    page: { type: 'number' },
   },
   $schema: 'http://json-schema.org/draft-07/schema#',
   additionalProperties: false,
