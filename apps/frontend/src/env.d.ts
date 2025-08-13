aa86394bc652b29a5be02d535a7d5c762ad66919
Aleksandr Dergunov <dergunovs@mail.ru>
2025-08-14 01:09:39 +0300

    chore: remove declare module vue

--- apps/frontend/src/env.d.ts
+++ apps/frontend/src/env.d.ts
@@ -3,13 +3,6 @@ import { DefineLocaleMessage } from 'vue-i18n';
 
 import ru from '@/common/locales/ru.json';
 
-declare module '*.vue' {
-  import type { DefineComponent } from 'vue';
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const component: DefineComponent<object, object, any>;
-  export default component;
-}
-
 declare module 'vue-i18n' {
   // eslint-disable-next-line @typescript-eslint/no-type-alias
   type TMessageSchema = typeof ru;
