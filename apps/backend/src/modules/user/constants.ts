48b5467882053d44a2567f5bcd83e0dc9f97fffb
Aleksandr Dergunov <dergunovs@mail.ru>
2025-09-21 12:03:09 +0300

    fix: template edit isWeightsRequired populate

--- apps/backend/src/modules/user/constants.ts
+++ apps/backend/src/modules/user/constants.ts
@@ -4,7 +4,7 @@ export const USER_POPULATE: IPopulate[] = [
   { path: 'equipments.equipment' },
   {
     path: 'templates.exercises.exercise',
-    select: '_id title title_en muscles createdBy isWeights equipmentForWeight',
+    select: '_id title title_en muscles createdBy isWeights isWeightsRequired equipmentForWeight',
     populate: [
       { path: 'createdBy', select: '_id name email' },
       { path: 'muscles', select: '_id title color' },
