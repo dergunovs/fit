e43b877aa96d644a1d14fbb952517b7e91ea957b
Aleksandr Dergunov <dergunovs@mail.ru>
2025-11-02 22:45:59 +0300

    chore: reduce apk file size

--- apps/backend/src/modules/equipment/routes.ts
+++ apps/backend/src/modules/equipment/routes.ts
@@ -30,7 +30,7 @@ export default async function (fastify: IFastifyInstance) {
     async function (_request, reply) {
       const data = await equipmentService.getAll();
 
-      reply.header('Cache-Control', 'public, max-age=604800');
+      reply.header('Cache-Control', 'public, max-age=1000000');
       reply.code(200).send(data);
     }
   );
--- apps/backend/src/modules/muscle/routes.ts
+++ apps/backend/src/modules/muscle/routes.ts
@@ -29,7 +29,7 @@ export default async function (fastify: IFastifyInstance) {
     async function (_request, reply) {
       const data = await muscleService.getAll();
 
-      reply.header('Cache-Control', 'public, max-age=604800');
+      reply.header('Cache-Control', 'public, max-age=1000000');
       reply.code(200).send(data);
     }
   );
