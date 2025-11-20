99a3b712d152f7f438164db8bff37bf89feac087
Aleksandr Dergunov <dergunovs@mail.ru>
2025-11-20 09:28:32 +0300

    chore: add cors

--- apps/backend/src/modules/common/plugins/cors.ts
+++ apps/backend/src/modules/common/plugins/cors.ts
@@ -3,7 +3,7 @@ import cors from '@fastify/cors';
 
 export default fp(async function (fastify) {
   fastify.register(cors, {
-    origin: '*',
+    origin: [`${process.env.APP_URL}`, 'http://tauri.localhost'],
     methods: 'GET,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
     allowedHeaders: 'Content-Type,Authorization',
