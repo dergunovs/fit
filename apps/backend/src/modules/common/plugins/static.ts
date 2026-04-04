2d141e4d134a9b380f3a75c404581f496856cf70
Aleksandr Dergunov <dergunovs@mail.ru>
2026-04-04 22:55:38 +0300

    fix: upload path

--- apps/backend/src/modules/common/plugins/static.ts
+++ apps/backend/src/modules/common/plugins/static.ts
@@ -1,16 +1,11 @@
 import path from 'node:path';
-import { fileURLToPath } from 'node:url';
 import { mkdir } from 'node:fs/promises';
 import fp from 'fastify-plugin';
 import staticF from '@fastify/static';
 
-const dirname = path.dirname(fileURLToPath(import.meta.url));
-const uploadDir = path.join(dirname, '../../../../upload');
+const uploadDir = path.join(process.cwd(), 'upload');
 
 export default fp(async function (fastify) {
   await mkdir(uploadDir, { recursive: true });
-  fastify.register(staticF, {
-    root: uploadDir,
-    prefix: '/upload',
-  });
+  fastify.register(staticF, { root: uploadDir, prefix: '/upload' });
 });
